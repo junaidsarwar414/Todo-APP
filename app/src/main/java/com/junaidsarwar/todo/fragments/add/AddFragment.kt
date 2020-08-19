72e87e7d389a7991637be7be64929268106843ec
Junaid Sarwar <junaid.sarwar414@gmail.com>
2020-08-20 02:27:20 +0500

    UI f TODO App

---
+++ app/src/main/java/com/junaidsarwar/todo/fragments/add/AddFragment.kt
@@ -0,0 +1,21 @@
+package com.junaidsarwar.todo.fragments.add
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import com.junaidsarwar.todo.R
+
+class AddFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        setHasOptionsMenu(true)
+        return inflater.inflate(R.layout.fragment_add, container, false)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.add_fragment_menu,menu)
+    }
+}
